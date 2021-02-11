f7b6970352934fead53ec458f3f65c87a1f3aecd
TheBoringDude <iamcoderx@gmail.com>
2021-02-11 22:25:15 +0800

    test: args

---
+++ args_test.v
@@ -0,0 +1,32 @@
+module vargus
+
+struct TestCase {
+	case string
+	exp_out bool
+}
+
+
+fn test_args_has_hyphen_dash() {
+	test_cases := [
+		TestCase {
+			case: '-r'
+			exp_out: true
+		},
+		TestCase {
+			case: '--r'
+			exp_out: true
+		},
+		TestCase {
+			case: '---r'
+			exp_out: false
+		},
+		TestCase {
+			case: 'r'
+			exp_out: false
+		},
+	]
+
+	for t in test_cases {
+		assert args_has_hyphen_dash(t.case) == t.exp_out
+	}
+}
