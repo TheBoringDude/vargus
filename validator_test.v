fb32e9e2985b2502a550b69b37aba6a207f0e4ff
TheBoringDude <iamcoderx@gmail.com>
2021-02-11 22:19:17 +0800

    Improve float validation

--- validator_test.v
+++ validator_test.v
@@ -1,39 +1,75 @@
 module vargus
 
+struct TestCase {
+	case string
+	exp_out bool
+}
+
 // TEST CASES FOR INT_VALIDATOR
-fn test_int_validator_basic() {
-	x := '1'
-	assert int_validator(x) == true
-}
-fn test_int_validator_negative() {
-	x := '-1'
-	assert int_validator(x) == true
-}
-fn test_int_validator_mis1() {
-	x := '-1.0'
-	assert int_validator(x) == false
-}
-fn test_int_validator_mis2() {
-	x := '1-0'
-	assert int_validator(x) == false
+fn test_int_validator() {
+	test_cases := [
+		TestCase {
+			case: '1'
+			exp_out: true
+		},
+		TestCase {
+			case: '-1'
+			exp_out: true
+		},
+		TestCase {
+			case: '-1.0'
+			exp_out: false
+		},
+		TestCase {
+			case: '1-0'
+			exp_out: false
+		},
+	]
+	
+	for t in test_cases {
+		assert int_validator(t.case) == t.exp_out
+	}
 }
 // TEST CASES FOR INT_VALIDATOR
 
 // TEST CASES FOR FLOAT_VALIDATOR
 fn test_float_validator_basic() {
-	x := '1.0'
-	assert float_validator(x) == true
+	test_cases := [
+		TestCase {
+			case: '1.0'
+			exp_out: true
+		},
+		TestCase {
+			case: '-1.0'
+			exp_out: true
+		},
+		TestCase {
+			case: '-1..0'
+			exp_out: false
+		},
+		TestCase {
+			case: '1.-0'
+			exp_out: false
+		},
+		TestCase {
+			case: '1e9'
+			exp_out: true
+		},
+		TestCase {
+			case: '1-e9'
+			exp_out: false
+		},
+		TestCase {
+			case: '1e-9'
+			exp_out: true
+		},
+		TestCase {
+			case: '-1.0-e-9'
+			exp_out: false
+		},
+	]
+
+	for t in test_cases {
+		assert float_validator(t.case) == t.exp_out
+	}
 }
-fn test_float_validator_negative() {
-	x := '-1.0'
-	assert float_validator(x) == true
-}
-fn test_float_validator_mis1() {
-	x := '-1..0'
-	assert float_validator(x) == false
-}
-fn test_float_validator_mis2() {
-	x := '1.-0'
-	assert float_validator(x) == false
-}
-// TEST CASES FOR FLOAT_VALIDATOR
