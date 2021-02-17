9e84b0157063f0f167d01876823db929f7fd3603
TheBoringDude <iamcoderx@gmail.com>
2021-02-17 19:54:05 +0800

    test: add test case for parse_flags

---
+++ parse_test.v
@@ -0,0 +1,21 @@
+module vargus
+
+// test_parse_flags is a test for vargus' flag parser
+fn test_parse_flags() {
+	// define custom variables
+	mut c := &Commander{
+		command: 'root'
+	}
+
+	c.add_local_flag_bool(
+		name: 'verbose'
+		short_arg: 'v'
+		required: false
+		default_value: false
+		help: 'set to verbose'
+	)
+
+	_, test_flag := c.parse_flags(['-v', 'true'], []FlagArgs{}, CommandConfig{})
+
+	assert test_flag[0].value == 'true' 
+}
