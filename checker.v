97aeb0564c6cc19812edfce22176f7f649e4a622
TheBoringDude <iamcoderx@gmail.com>
2021-02-20 22:14:27 +0800

    checker: minify code

--- checker.v
+++ checker.v
@@ -3,13 +3,13 @@ module vargus
 // command_checker is a utility cmdconfig checker which checks if important fields are not set
 fn command_checker(cmdConfig CmdConfig) {
 	// check if command name is set
-	if cmdConfig.command.trim_space() == '' {
+	if is_blank(cmdConfig.command) {
 		eprintln('\n ![invalid] please set a command name for your cli app')
 		exit(1)
 	}
 
 	// command description is required to be set
-	if cmdConfig.short_desc.trim_space() == '' && cmdConfig.long_desc.trim_space() == '' {
+	if is_blank(cmdConfig.short_desc) && is_blank(cmdConfig.long_desc) {
 		eprintln('\n ![invalid] a description should atleast be set, short or long')
 		exit(1)
 	}
@@ -18,8 +18,19 @@ fn command_checker(cmdConfig CmdConfig) {
 
 // flag_checker is a utlity flag checker which checks if important fiels are set
 fn flag_checker(flag_name string, flag_short_arg string) {
-	if flag_name.trim_space() == '' && flag_short_arg.trim_space() == '' {
+	if is_blank(flag_name) && is_blank(flag_short_arg) {
 		eprintln('\n ![invalid] you should set a flag name or a short_arg')
 		exit(1)
 	}
+}
+
+
+// check_if_blank checks if a string is just a blank
+fn is_blank(text string) bool {
+	// remove space and check
+	if text.trim_space() == '' {
+		return true
+	}
+	
+	return false
 }
